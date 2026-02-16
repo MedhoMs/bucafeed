c70c2f2926faafce4971684d04f4c024bce94ed6
Antonio <antoniomorera784@gmail.com>
2026-02-16 19:05:38 +0100

    primeras tablas 1

--- backend/database/migrations/2026_02_15_000000_create_usuarios_table.php
+++ backend/database/migrations/2026_02_15_000000_create_usuarios_table.php
@@ -19,13 +19,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
 
-            $table->integer('role');
+            $table->string('role'); // user, institution, student, teacher, admin, moderator
 
             $table->integer('reputation')->default(0);
             $table->foreignId('educational_center_id')->nullable()
                 ->constrained('educational_centers');
 
-            $table->string('dni')->unique();
+            $table->string('national_id')->unique(); // dni
 
             $table->enum('education_level', ['primary','secondary'])->nullable();
             $table->string('institution_name')->nullable();
@@ -39,6 +39,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('users');
     }
 };
