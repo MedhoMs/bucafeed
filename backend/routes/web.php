5787bdfd4f58aba07e4fdf09c177c268aee81fa6
Antonio <antoniomorera784@gmail.com>
2026-02-19 23:31:55 +0100

    arreglo 19

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,25 +11,7 @@ Route::get('/', function () {
 Route::get('/prueba', function () {
     return view('prueba');
 });
-Route::get('/db-check', function () {
-    try {
-        \Illuminate\Support\Facades\DB::connection()->getPdo();
-        $tables = \Illuminate\Support\Facades\DB::select('SHOW TABLES');
-        $tablesList = array_map(function($t) { return array_values((array)$t)[0]; }, $tables);
-        
-        return response()->json([
-            'status' => 'ok',
-            'message' => 'Database connected',
-            'tables' => $tablesList,
-            'user_count' => \App\Models\User::count(),
-        ]);
-    } catch (\Throwable $e) {
-        return response()->json([
-            'status' => 'error',
-            'message' => $e->getMessage(),
-        ], 500);
-    }
-});
+
 
 
 Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
@@ -47,6 +29,3 @@ Route::get('/users/show/{id}', [UserController::class, 'show'])->name('user.show
 
 Route::get('/users/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 Route::post('/users/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-
-
-
