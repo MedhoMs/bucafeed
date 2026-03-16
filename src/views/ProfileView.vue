fe1fd4e22df9c6bc283ca2364aa0af3c99e0c30d
MedhoMs <danielbucaloiu@gmail.com>
2026-03-16 11:34:48 +0000

    feat/correct username appears when login

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,8 @@
 <script setup>
 import NavBar from '@/components/NavBar/NavBar.vue';
-import SideBar from '@/components/SideBar.vue';
-import SearchBar from '@/components/SearchBar.vue';
 import ButtonTemplate from '@/components/buttons/ButtonTemplate.vue';
 import { useTranslations } from '@/composables/useTranslations'
+import { user } from '@/stores/auth'
 
 const { t } = useTranslations()
 
@@ -42,10 +41,10 @@ const usuario = {
                 <!-- Información del perfil -->
                 <div class="px-5 pb-2.5 -mt-2">
                     <div class="flex items-center gap-1">
-                        <h2 class="nombre text-2xl font-bold m-0 text-[#e7e9ea]">{{ usuario.nombre }}</h2>
+                        <h2 class="nombre text-2xl font-bold m-0 text-[#e7e9ea]">{{ user ? user.name : 'Usuario' }}</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#179cf0" class="icon icon-tabler icons-tabler-filled icon-tabler-rosette-discount-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.01 2.011a3.2 3.2 0 0 1 2.113 .797l.154 .145l.698 .698a1.2 1.2 0 0 0 .71 .341l.135 .008h1a3.2 3.2 0 0 1 3.195 3.018l.005 .182v1c0 .27 .092 .533 .258 .743l.09 .1l.697 .698a3.2 3.2 0 0 1 .147 4.382l-.145 .154l-.698 .698a1.2 1.2 0 0 0 -.341 .71l-.008 .135v1a3.2 3.2 0 0 1 -3.018 3.195l-.182 .005h-1a1.2 1.2 0 0 0 -.743 .258l-.1 .09l-.698 .697a3.2 3.2 0 0 1 -4.382 .147l-.154 -.145l-.698 -.698a1.2 1.2 0 0 0 -.71 -.341l-.135 -.008h-1a3.2 3.2 0 0 1 -3.195 -3.018l-.005 -.182v-1a1.2 1.2 0 0 0 -.258 -.743l-.09 -.1l-.697 -.698a3.2 3.2 0 0 1 -.147 -4.382l.145 -.154l.698 -.698a1.2 1.2 0 0 0 .341 -.71l.008 -.135v-1l.005 -.182a3.2 3.2 0 0 1 3.013 -3.013l.182 -.005h1a1.2 1.2 0 0 0 .743 -.258l.1 -.09l.698 -.697a3.2 3.2 0 0 1 2.269 -.944zm3.697 7.282a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>
                     </div>
-                    <p class="nombre-usuario text-[#8b98a5] text-base my-0.5 mx-0">{{ usuario.usuarioNombre }}</p>
+                    <p class="nombre-usuario text-[#8b98a5] text-base my-0.5 mx-0">{{ user ? user.email : 'Usuario' }}</p>
                     <p class="seguidores text-[#8b98a5] text-sm mt-2">
                         <span class="numero font-bold text-[#e7e9ea]">{{ usuario.seguidores }}</span> 
                         {{ t.profile.followers }} · 
