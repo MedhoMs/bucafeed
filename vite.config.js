f762e1c26856eb3b17980e0ebe8125a5eef8338d
Antonio <antoniomorera784@gmail.com>
2026-04-14 20:42:22 +0200

    fix tailwind

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,7 @@ export default defineConfig({
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/styles/variables.scss";'
-      }
+      scss: {}
     }
   }
 })
