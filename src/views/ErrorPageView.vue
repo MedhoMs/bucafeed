63195b4d5ae851ec3c58374f4621a0d9b6765bf7
MedhoMs <danielbucaloiu@gmail.com>
2026-05-13 13:09:57 +0100

    added card image & pending an improvement

--- src/views/ErrorPageView.vue
+++ src/views/ErrorPageView.vue
@@ -13,11 +13,35 @@
                 <h1 class="text-6xl font-bold">404</h1>
                 <p class="text-2xl">Esta página se ha perdido en el oceáno</p>
             </div>
-            <img src="/public/logoTelamon.png" alt="">
+            <img src="/public/logoTelamon.png" alt="Barco Telamon">
         </section>
     </main>
 </template>
 
 <style scoped>
+    section {
+        background-image: url("/public/telamonBarco.webp");
+        background-size: cover;
+        background-repeat: no-repeat;
+        position: relative;
+        border-radius: 0.75rem;
+        overflow: hidden;
+    }
 
+    section::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        pointer-events: none;
+        border-radius: 0.75rem;
+    }
+
+    section > * {
+        position: relative;
+        z-index: 1;
+    }
 </style>
